bd3f39cc5c0ee7df200cc4b2caf7dfbc645935d4
Mrityunjay <mjguptacse@gmail.com>
2024-08-07 12:20:29 +0530

    Completed Types and Interface

--- src/test.ts
+++ src/test.ts
@@ -30,3 +30,102 @@ trial(sum);
 //1.First add root directory to src and Out directory to dist
 //2. No implicit any because it gives all variables and function to any type by default
 //3. ///Enable Remove comment:true  it will not allow comments to run that code
+
+//Object Playing
+function isAble(user: { firstName: string; lastName: string; age: number }) {
+  //if we use this object data in many places then we have to write this type of data in all place where we use object
+  if (user.age > 18) return true;
+  else return false;
+}
+const result = isAble({
+  firstName: "Mrityunjay",
+  lastName: "Gupta",
+  age: 21,
+});
+console.log(result);
+//To overcome this we use interface where we define the type of object in one place and use it everywhere
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email?: string; //optional argument
+}
+
+function isAble2(user: User) {
+  if (user.age > 18) return true;
+  else return false;
+}
+function greet2(user: User) {
+  console.log("Hi There " + user.firstName);
+}
+
+greet2({
+  firstName: "Mrityunjay",
+  lastName: "Gupta",
+  age: 21,
+});
+const result2 = isAble2({
+  firstName: "Mrityunjay",
+  lastName: "Gupta",
+  age: 21,
+});
+console.log(result2);
+
+//Type almost the same as interface it cant be implemented as interfaces
+type User2 = {
+  firsName: string;
+  lastName: string;
+  age: number;
+};
+//First Difference is Union
+//Print the id of a user which can be number or string
+function printId(id: number | string) {
+  //But this syntax is not readable or optimized so we use type in it
+  console.log("Id of user is :" + id);
+}
+printId(8);
+printId("12");
+
+//This sytax looks clean and optimized {We cant do this through interface}
+type printArg = string | number;
+
+function printId2(id: printArg) {
+  //But this syntax is not readable or optimized so we use type in it
+  console.log("Id of user is :" + id);
+}
+printId2(5);
+printId2("10");
+
+//Second difference that is Intersection
+//If you have to use two type simultaneously
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type Manager = {
+  name: string;
+  department: string;
+};
+
+type TeamLead = Employee & Manager;
+
+const teamLead: TeamLead = {
+  name: "Mrityunjay",
+  startDate: new Date(),
+  department: "Software Developer",
+};
+
+function printDetails(team: TeamLead) {
+  console.log(team.name);
+  console.log(team.startDate);
+  console.log(team.department);
+}
+
+printDetails(teamLead);
+
+//Array in tyescript
+//There are two ways through we can aceess array into typescript
+//First is through interface
+//Function to return max values in arary
